1584feb1ceea0f7ed3123fe1c40b4d8a4df3087d
Andrzej Kurek <andrzejku@gmail.com>
2023-01-27 01:21:12 +0100

    Text12 +500

--- src/text/text12.asm
+++ src/text/text12.asm
@@ -503,20 +503,19 @@ MirrorMoveName: ; 614c3 (18:54c3)
 	done
 
 PidgeottosMirrorMoveDescription: ; 614d0 (18:54d0)
-	text "If Pidgeotto was attacked last turn,"
-	line "do the final result of that attack"
-	line "on Pidgeotto to the Defending"
-	line "Pokémon."
+	text "Jeśli Pidgeotto był zaatakowany"
+	line "w ostatniej turze, ten atak zada"
+	line "tyle samo obrażeń wrogiemu"
+	line "Pokémonowi."
 	done
 
 BirdName: ; 61540 (18:5540)
-	text "Bird"
+	text "Ptak"
 	done
 
 PidgeottoDescription: ; 61546 (18:5546)
-	text "Very protective of its sprawling"
-	line "territory, this Pokémon will"
-	line "fiercely peck at any intruder."
+	text "Chroni swego rozległego terytorium."
+	line "Zaatakuje dziobem każdego intruza."
 	done
 
 PidgeotName: ; 615a4 (18:55a4)
@@ -524,55 +523,53 @@ PidgeotName: ; 615a4 (18:55a4)
 	done
 
 SlicingWindName: ; 615ad (18:55ad)
-	text "Slicing Wind"
+	text "Tnący Wiatr"
 	done
 
 SlicingWildDescription: ; 615bb (18:55bb)
-	text "Does 30 damage to 1 of your"
-	line "opponent's Pokémon chosen at random."
-	line "Don't apply Weakness and Resistance"
-	line "for this attack. (Any other effects"
-	line "that would happen after applying"
-	line "Weakness and Resistance still"
-	line "happen.)"
+	text "Zadaj 30 obrażeń losowemu Pokémonowi"
+	line "wroga. Nie uwzględniaj słabości"
+	line "i odporności dla tego ataku. Każdy"
+	line "inny efekt, który by nastąpił po"
+	line "uwzględnieniu słabości i odporności"
+	line "nadal wystąpi."
 	done
 
 GaleName: ; 6168d (18:568d)
-	text "Gale"
+	text "Wichura"
 	done
 
 GaleDescription: ; 61693 (18:5693)
-	text "Switch Pidgeot with 1 of your"
-	line "Benched Pokémon chosen at random."
-	line "If your opponent has any Benched"
-	line "Pokémon, switch the Defending"
-	line "Pokémon with 1 of them chosen at"
-	line "random. (Do the damage before"
-	line "switching the Pokémon.)"
+	text "Zamień Pidgeota z losowym Pokémonem"
+	line "z ławki. Jeśli twój przeciwnik ma"
+	line "Pokémony na ławce, zamień jego"
+	line "aktywnego Pokémona z losowym"
+	line "Pokémonem z jego ławki."
+	line "Obrażenia zostaną zadane przed"
+	line "podmianą Pokémona."
 	done
 
 Pidgeot1Description: ; 6176a (18:576a)
-	text "This Pokémon flies at Mach 2 speed,"
-	line "seeking prey. Its large talons are"
-	line "feared as wicked weapons."
+	text "Ten Pokémon lata z prędkością"
+	line "2 Machów, szukając ofiary. Jego"
+	line "szpony stanowią straszną broń."
 	done
 
 HurricaneName: ; 617cc (18:57cc)
-	text "Hurricane"
+	text "Huragan"
 	done
 
 HurricaneDescription: ; 617d7 (18:57d7)
-	text "Unless this attack Knocks Out the"
-	line "Defending Pokémon, return the"
-	line "Defending Pokémon and all cards"
-	line "attached to it to your opponent's"
-	line "hand."
+	text "O ile ten ruch nie znokautuje"
+	line "atakowanego Pokémona, zwróć go razem"
+	line "ze wszystkimi dołączonymi kartami"
+	line "na rękę przeciwnika."
 	done
 
 Pidgeot2Description: ; 61860 (18:5860)
-	text "When hunting, it skims the surface"
-	line "of water at high speed to pick off"
-	line "unwary prey such as Magikarp."
+	text "W trakcie polowania lata tuż nad"
+	line "powierzchnią wody z dużą prędkością,"
+	line "porywając ofiary, np. Magikarpy."
 	done
 
 RattataName: ; 618c5 (18:58c5)
@@ -580,13 +577,13 @@ RattataName: ; 618c5 (18:58c5)
 	done
 
 RatName: ; 618ce (18:58ce)
-	text "Rat"
+	text "Szczur"
 	done
 
 RattataDescription: ; 618d3 (18:58d3)
-	text "Bites anything when it attacks."
-	line "Small and very quick, it is a common"
-	line "sight in many places."
+	text "W trakcie ataku gryzie co popadnie."
+	line "Mały i bardzo szybki, często"
+	line "widziany w wielu miejscach."
 	done
 
 RaticateName: ; 6192f (18:592f)
@@ -594,20 +591,20 @@ RaticateName: ; 6192f (18:592f)
 	done
 
 SuperFangName: ; 61939 (18:5939)
-	text "Super Fang"
+	text "Super Kieł"
 	done
 
 SuperFangDescription: ; 61945 (18:5945)
-	text "Does damage to the Defending Pokémon"
-	line "equal to half the Defending"
-	line "Pokémon's remaining HP (rounded up"
-	line "to the nearest 10)."
+	text "Zadaje obrażenia równe połowy"
+	line "pozostałego życia atakowanego"
+	line "Pokémona (zaokrąglone do"
+	line "najbliższych 10)."
 	done
 
 RaticateDescription: ; 619be (18:59be)
-	text "It uses its whiskers to maintain its"
-	line "balance. It seems to slow down if"
-	line "they are cut off."
+	text "Używa wąsów do utrzymania balansu."
+	line "Ma zdecydowanie wolniejsze ruchy"
+	line "jeśli je straci."
 	done
 
 SpearowName: ; 61a18 (18:5a18)
@@ -615,19 +612,20 @@ SpearowName: ; 61a18 (18:5a18)
 	done
 
 PeckName: ; 61a21 (18:5a21)
-	text "Peck"
+	text "Dziobnięcie"
 	done
 
 SpearowsMirrorMoveDescription: ; 61a27 (18:5a27)
-	text "If Spearow was attacked last turn,"
-	line "do the final result of that attack"
-	line "on Spearow to the Defending Pokémon."
+	text "Jeśli Spearow był zaatakowany"
+	line "w ostatniej turze, ten atak zada"
+	line "tyle samo obrażeń wrogiemu"
+	line "Pokémonowi."
 	done
 
 SpearowDescription: ; 61a93 (18:5a93)
-	text "Eats bugs in grassy areas. It has to"
-	line "flap its short wings at high speed"
-	line "to stay airborne."
+	text "Zjada robaki z trawiastych miejsc."
+	line "Musi bardzo szybko machać swymi"
+	line "krótkimi skrzydłami aby latać."
 	done
 
 FearowName: ; 61aee (18:5aee)
@@ -635,24 +633,24 @@ FearowName: ; 61aee (18:5aee)
 	done
 
 FearowsAgilityDescription: ; 61af6 (18:5af6)
-	text "Flip a coin. If heads, during your"
-	line "opponent's next turn, prevent all"
-	line "effects of attacks, including"
-	line "damage, done to Fearow."
+	text "Rzuć monetą . Pikachu: w kolejnej"
+	line "turze wroga zignoruj wszystkie"
+	line "efekty ataków, w tym obrażenia,"
+	line "zadawane Fearow'owi."
 	done
 
 DrillPeckName: ; 61b72 (18:5b72)
-	text "Drill Peck"
+	text "Wiercący Dziób"
 	done
 
 BeakName: ; 61b7e (18:5b7e)
-	text "Beak"
+	text "Dziób"
 	done
 
 FearowDescription: ; 61b84 (18:5b84)
-	text "With its huge and magnificent wings,"
-	line "it can keep aloft without ever"
-	line "having to land for rest."
+	text "Dzięki swym wielkim i majestatycznym"
+	line "skrzydłom jest w stanie unosić się"
+	line "w powietrzu bez żadnego odpoczynku."
 	done
 
 ClefairyName: ; 61be2 (18:5be2)
@@ -660,30 +658,29 @@ ClefairyName: ; 61be2 (18:5be2)
 	done
 
 SingName: ; 61bec (18:5bec)
-	text "Sing"
+	text "Śpiew"
 	done
 
 MetronomeName: ; 61bf2 (18:5bf2)
-	text "Metronome"
+	text "Metronom"
 	done
 
 ClefairysMetronomeDescription: ; 61bfd (18:5bfd)
-	text "Choose 1 of the Defending Pokémon's"
-	line "attacks. Metronome copies that"
-	line "attack except for its Energy costs."
-	line "(No matter what type the Defending"
-	line "Pokemon is, Clefairy's type is"
-	line "still Colorless.)"
+	text "Wybierz jeden z ataków wrogiego"
+	line "Pokémona. Metronom kopiuje ten atak,"
+	line "oprócz kosztu Energii. Obojętnie"
+	line "jakiego typu jest atakowany Pokémon,"
+	line "Clefairy nadal pozostaje bezbarwny."
 	done
 
 FairyName: ; 61cb9 (18:5cb9)
-	text "Fairy"
+	text "Wróżka"
 	done
 
 ClefairyDescription: ; 61cc0 (18:5cc0)
-	text "Its magical and cute appeal has many"
-	line "admirers. It is rare and found only"
-	line "in certain areas."
+	text "Jego magiczny urok ma licznych"
+	line "wielbicieli. Bardzo rzadki Pokémon,"
+	line "wiele czasu spędza w ukryciu."
 	done
 
 ClefableName: ; 61d1c (18:5d1c)
@@ -691,25 +688,24 @@ ClefableName: ; 61d1c (18:5d1c)
 	done
 
 ClefablesMetronomeDescription: ; 61d26 (18:5d26)
-	text "Choose 1 of the Defending Pokémon's"
-	line "attacks. Metronome copies that"
-	line "attack except for its Energy costs."
-	line "(No matter what type the Defending"
-	line "Pokémon is, Clefable's type is"
-	line "still Colorless.)"
+	text "Wybierz jeden z ataków wrogiego"
+	line "Pokémona. Metronom kopiuje ten atak,"
+	line "oprócz kosztu Energii. Obojętnie"
+	line "jakiego typu jest atakowany Pokémon,"
+	line "Clefable nadal pozostaje bezbarwny."
 	done
 
 ClefablesMinimizeDescription: ; 61de2 (18:5de2)
-	text "All damage done by attacks to"
-	line "Clefable during your opponent's next"
-	line "turn is reduced by 20 (after"
-	line "applying Weakness and Resistance)."
+	text "Wszystkie obrażenia, które Clefable"
+	line "otrzyma z ataków w następnej turze,"
+	line "są zmniejszone o 20 (po policzeniu"
+	line "słabości i odporności)."
 	done
 
 ClefableDescription: ; 61e66 (18:5e66)
-	text "A timid Fairy Pokémon that is rarely"
-	line "seen. It will run and hide the"
-	line "moment it senses people."
+	text "Nieśmiały i rzadki Pokémon-Wróżka."
+	line "Stara się chować gdy tylko wyczuje"
+	line "ludzką obecność w okolicy."
 	done
 
 JigglypuffName: ; 61ec4 (18:5ec4)
@@ -717,70 +713,71 @@ JigglypuffName: ; 61ec4 (18:5ec4)
 	done
 
 FirstAidName: ; 61ed0 (18:5ed0)
-	text "First Aid"
+	text "Pierwsza Pomoc"
 	done
 
 FirstAidDescription: ; 61edb (18:5edb)
-	text "Remove 1 damage counter from"
-	line "Jigglypuff."
+	text "Usuń 1 licznik obrażeń"
+	line "z Jigglypuff'a."
 	done
 
 DoubleEdgeName: ; 61f05 (18:5f05)
-	text "Double-edge"
+	text "Obosieczny atak"
 	done
 
 JigglypuffsDoubleEdgeDescription: ; 61f12 (18:5f12)
-	text "Jigglypuff does 20 damage to itself."
+	text "Jigglypuff zadaje 20 obrażeń sobie."
 	done
 
 BalloonName: ; 61f38 (18:5f38)
-	text "Balloon"
+	text "Balon"
 	done
 
 JigglypuffLv12Description: ; 61f41 (18:5f41)
-	text "When its huge eyes light up, it"
-	line "sings a mysteriously soothing"
-	line "melody that lulls its enemies to"
-	line "sleep."
+	text "Gdy zaświecą mu się oczy, zaczyna"
+	line "śpiewać tajemniczo uspokajającą"
+	line "piosenkę, którą może uśpić swych"
+	line "wrogów."
 	done
 
 FriendshipSongName: ; 61fa8 (18:5fa8)
-	text "Friendship Song"
+	text "Pieśń Przyjaźni"
 	done
 
 FriendshipSongDescription: ; 61fb9 (18:5fb9)
-	text "Flip a coin. If heads, put a Basic"
-	line "Pokémon card chosen at random from"
-	line "your deck onto your Bench. (You"
-	line "can't use this attack if your Bench"
-	line "is full.)"
+	text "Rzuć monetą . Pikachu: umieść"
+	line "losową, podstawową kartę Pokémon"
+	line "z talii na swojej ławce (Nie"
+	line "możesz użyć tego ataku jeśli masz"
+	line "pełną ławkę)."
 	done
 
 ExpandName: ; 6204e (18:604e)
-	text "Expand"
+	text "Nadęcie"
 	done
 
 ExpandDescription: ; 62056 (18:6056)
-	text "All damage done to Jigglypuff during"
-	line "your opponent's next turn is reduced"
-	line "by 10 (after applying Weakness and"
-	line "Resistance)."
+	text "Wszystkie obrażenia zadane"
+	line "Jigglypuff'owi w kolejnej turze"
+	line "wroga są zredukowane o 10 (po"
+	line "uwzględnieniu słabości i odporności)"
 	done
 
 JigglypuffLv13Description: ; 620d1 (18:60d1)
-	text "Uses its alluring eyes to enrapture"
-	line "its foe. It then sings a pleasing"
-	line "melody that lulls the foe to sleep."
+	text "Używa swych powabnych oczu, aby"
+	line "oczarować przeciwnika. Tuż po tym"
+	line "usypia go, śpiewając kołysankę."
 	done
 
 LullabyName: ; 6213c (18:613c)
-	text "Lullaby"
+	text "Kołysanka"
 	done
 
 JigglypuffLv14Description: ; 62145 (18:6145)
-	text "When its huge eyes light up, it"
-	line "sings a mysteriously soothing melody"
-	line "that lulls its enemies to sleep."
+	text "Gdy zaświecą mu się oczy, zaczyna"
+	line "śpiewać tajemniczo uspokajającą"
+	line "piosenkę, którą może uśpić swych"
+	line "wrogów."
 	done
 
 WigglytuffName: ; 621ac (18:61ac)
@@ -788,18 +785,18 @@ WigglytuffName: ; 621ac (18:61ac)
 	done
 
 DoTheWaveName: ; 621b8 (18:61b8)
-	text "Do the Wave"
+	text "Meksykańska Fala"
 	done
 
 DoTheWaveDescription: ; 621c5 (18:61c5)
-	text "Does 10 damage plus 10 more damage"
-	line "for each of your Benched Pokémon."
+	text "Zadaje 10 obrażeń plus 10 więcej"
+	line "za każdego Pokémona na twej ławce."
 	done
 
 WigglytuffDescription: ; 6220b (18:620b)
-	text "The body is soft and rubbery. When"
-	line "angered, it will suck in air and"
-	line "inflate itself to an enormous size."
+	text "Ma miękkie, gumowe ciało."
+	line "Zezłoszczony wciąga powietrze"
+	line "i nadyma się do wielkich rozmiarów."
 	done
 
 MeowthName: ; 62274 (18:6274)
@@ -807,41 +804,42 @@ MeowthName: ; 62274 (18:6274)
 	done
 
 CatPunchName: ; 6227c (18:627c)
-	text "Cat Punch"
+	text "Kocie Pacnięcie"
 	done
 
 CatPunchDescription: ; 62287 (18:6287)
-	text "Does 20 damage to 1 of your"
-	line "opponent's Pokémon chosen at random."
-	line "Don't apply Weakness and Resistance"
-	line "for this attack. (Any other effects"
-	line "that would happen after applying"
-	line "Weakness and Resistance still"
-	line "happen.)"
+	text "Zadaj 20 obrażeń losowemu Pokémonowi"
+	line "przeciwnika. Nie uwzględniaj"
+	line "słabości i odporności dla tego"
+	line "ataku. Wszystkie inne efekty, które"
+	line "miałyby miejsce po liczeniu"
+	line "słabości i odporności, nadal"
+	line "wystąpią."
 	done
 
 ScratchCatName: ; 62359 (18:6359)
-	text "Scratch Cat"
+	text "Drapiący Kot"
 	done
 
 MeowthLv14Description: ; 62366 (18:6366)
-	text "Appears to be more active at night."
-	line "It loves round and shiny things, so"
-	line "it can't stop from picking them up."
+	text "Jest bardziej aktywny nocą."
+	line "Uwielbia błyszczące, okrągłe rzeczy,"
+	line "nie może przestać ich zbierać."
 	done
 
 PayDayName: ; 623d3 (18:63d3)
-	text "Pay Day"
+	text "Wypłata"
 	done
 
 PayDayDescription: ; 623dc (18:63dc)
-	text "Flip a coin. If heads, draw a card."
+	text "Rzuć monetą ."
+	line "Pikachu: weź kartę z talii na rękę."
 	done
 
 MeowthLv15Description: ; 62401 (18:6401)
-	text "Adores circular objects. Wanders"
-	line "the streets on a nightly basis to"
-	line "look for dropped loose change."
+	text "Uwielbia okrągłe przedmioty. Włóczy"
+	line "się co noc po ulicach szukając"
+	line "upuszczonych drobniaków."
 	done
 
 PersianName: ; 62464 (18:6464)
@@ -849,27 +847,27 @@ PersianName: ; 62464 (18:6464)
 	done
 
 PounceName: ; 6246d (18:646d)
-	text "Pounce"
+	text "Przygniecenie"
 	done
 
 PounceDescription: ; 62475 (18:6475)
-	text "If the Defending Pokémon attacks"
-	line "Persian during your opponent's next"
-	line "turn, any damage done by the attack"
-	line "is reduced by 10 (after applying"
-	line "Weakness and Resistance). (Benching"
-	line "or evolving either Pokémon ends this"
-	line "effect.)"
+	text "Jeśli atakowany Pokémon zaatakuje"
+	line "Persiana w kolejnej turze, zmniejsz"
+	line "te obrażenia o 10 (po uwzględnieniu"
+	line "słabości i odporności) (ewoluowanie"
+	line "któregokolwiek z aktywnych Pokémonów"
+	line "lub umieszczenie ich na ławce"
+	line "anuluje ten efekt)."
 	done
 
 ClassyCatName: ; 62552 (18:6552)
-	text "Classy Cat"
+	text "Kot z Klasą"
 	done
 
 PersianDescription: ; 6255e (18:655e)
-	text "Although its fur has many admirers,"
-	line "it is tough to raise as a pet"
-	line "because of its fickle meanness."
+	text "Choć jest wielbiony za wyjątkowe"
+	line "futro, nie jest dobrym zwierzakiem -"
+	line "bywa wredny i ma chwiejny nastrój."
 	done
 
 FarfetchdName: ; 625c1 (18:65c1)
@@ -877,30 +875,28 @@ FarfetchdName: ; 625c1 (18:65c1)
 	done
 
 LeekSlapName: ; 625cd (18:65cd)
-	text "Leek Slap"
+	text "Cios Porem"
 	done
 
 LeekSlapDescription: ; 625d8 (18:65d8)
-	text "Flip a coin. If tails, this attack"
-	line "does nothing. Either way, you can't"
-	line "use this attack again as long as"
-	line "Farfetch'd stays in play (even"
-	line "putting Farfetch'd on the Bench"
-	line "won't let you use it again)."
+	text "Rzuć monetą . Reszka: atak nic nie"
+	line "robi. Możesz użyć ataku tylko raz."
+	line "Nawet odstawienie Farfetch'd'a na"
+	line "ławkę nie pozwoli Ci go znów użyć."
 	done
 
 PotSmashName: ; 6269d (18:669d)
-	text "Pot Smash"
+	text "łupnięcie"
 	done
 
 WildDuckName: ; 626a8 (18:66a8)
-	text "Wild Duck"
+	text "Dzika Kaczka"
 	done
 
 FarfetchdDescription: ; 626b3 (18:66b3)
-	text "The sprig of green onions it holds"
-	line "is its weapon. This sprig is used"
-	line "much like a metal sword."
+	text "Trzyma w ręce pora, którym atakuje."
+	line "Używa go jak miecza, tnąc i kłując"
+	line "przeciwników."
 	done
 
 DoduoName: ; 62712 (18:6712)
@@ -908,17 +904,17 @@ DoduoName: ; 62712 (18:6712)
 	done
 
 FuryAttackName: ; 62719 (18:6719)
-	text "Fury Attack"
+	text "Napad Furii"
 	done
 
 TwinBirdName: ; 62726 (18:6726)
-	text "Twin Bird"
+	text "Bliźnioptak"
 	done
 
 DoduoDescription: ; 62731 (18:6731)
-	text "A bird that makes up for its poor"
-	line "flying with its fast foot speed."
-	line "Leaves giant footprints."
+	text "Wielki ptak, który bardzo słabo"
+	line "lata, ale za to szybko biega."
+	line "Zostawia duże ślady stóp."
 	done
 
 DodrioName: ; 6278e (18:678e)
@@ -926,28 +922,28 @@ DodrioName: ; 6278e (18:678e)
 	done
 
 RetreatAidName: ; 62796 (18:6796)
-	text "Retreat Aid"
+	text "Pomoc w Ucieczce"
 	done
 
 RetreatAidDescription: ; 627a3 (18:67a3)
-	text "As long as Dodrio is Benched, pay"
-	line "<COLORLESS> less to retreat your Active"
-	line "Pokémon."
+	text "Póki Dodrio jest na ławce, płać"
+	line "<COLORLESS> mniej za ucieczkę"
+	line "aktywnego Pokémona."
 	done
 
 DodriosRageDescription: ; 627ee (18:67ee)
-	text "Does 10 damage plus 10 more damage"
-	line "for each damage counter on Dodrio."
+	text "Zadaj 10 obrażeń plus 10 dodatkowych"
+	line "za każdy licznik obrażeń na Dodrio."
 	done
 
 TriplebirdName: ; 62835 (18:6835)
-	text "Triplebird"
+	text "Trójptak"
 	done
 
 DodrioDescription: ; 62841 (18:6841)
-	text "Uses its three brains to execute"
-	line "complex plans. While two heads"
-	line "sleep, one head stays awake."
+	text "Używa trzech mózgów do obmyślania"
+	line "skomplikowanych planów. Gdy dwie"
+	line "głowy śpią, jedna czuwa."
 	done
 
 LickitungName: ; 6289f (18:689f)
@@ -955,17 +951,17 @@ LickitungName: ; 6289f (18:689f)
 	done
 
 TongueWrapName: ; 628aa (18:68aa)
-	text "Tongue Wrap"
+	text "Owinięcie Jęzorem"
 	done
 
 LickingName: ; 628b7 (18:68b7)
-	text "Licking"
+	text "Liżący"
 	done
 
 LickitungDescription: ; 628c0 (18:68c0)
-	text "Its tongue can be extended like a"
-	line "chameleon's. It leaves a stinging"
-	line "sensation when it licks enemies."
+	text "Jego język wydłuża się jak język"
+	line "kameleona. Polizanie przez niego"
+	line "powoduje silne kłucie i pieczenie."
 	done
 
 ChanseyName: ; 62926 (18:6926)
@@ -973,24 +969,25 @@ ChanseyName: ; 62926 (18:6926)
 	done
 
 ScrunchName: ; 6292f (18:692f)
-	text "Scrunch"
+	text "Zgrzyt"
 	done
 
 ScrunchDescription: ; 62938 (18:6938)
-	text "Flip a coin. If heads, prevent all"
-	line "damage done to Chansey during your"
-	line "opponent's next turn. (Any other"
-	line "effects of attacks still happen.)"
+	text "Rzuć monetą. Pikachu: zignoruj"
+	line "wszystkie obrażenia zadane Chansey"
+	line "w następnej turze przeciwnika."
+	line "Wszystkie inne efekty ataków nadal"
+	line "wystąpią."
 	done
 
 ChanseysDoubleEdgeDescription: ; 629c2 (18:69c2)
-	text "Chansey does 80 damage to itself."
+	text "Chansey zada sobie sam 80 obrażeń."
 	done
 
 ChanseyDescription: ; 629e5 (18:69e5)
-	text "A rare and elusive Pokémon that is"
-	line "said to bring happiness to those"
-	line "who manage to catch it."
+	text "Rzadki i unikatowy Pokémon, ponoć"
+	line "przynosi radość i szczęście tym,"
+	line "którym uda się go złapać."
 	done
 
 KangaskhanName: ; 62a42 (18:6a42)
@@ -998,11 +995,11 @@ KangaskhanName: ; 62a42 (18:6a42)
 	done
 
 FetchName: ; 62a4e (18:6a4e)
-	text "Fetch"
+	text "Podaj"
 	done
 
 FetchDescription: ; 62a55 (18:6a55)
-	text "Draw a card."
+	text "Weź kartę z Talii na Rękę."
 	done
 
 CometPunchName: ; 62a63 (18:6a63)
